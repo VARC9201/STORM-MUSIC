eb18b7ff52a17dfbcde2e02c1fd72893c483b32a
Devilxd91 <159794569+Devilxd91@users.noreply.github.com>
2024-03-04 17:17:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/VARC9210/MUSIC",
+    "https://github.com/Devilxd91/MUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
